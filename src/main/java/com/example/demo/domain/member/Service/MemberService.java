7eeec06f48111d88aa19e0472b62951a45b3961f
ajunsu5@gmail.com <ajunsu5@gmail.com>
2024-07-02 14:31:21 +0900

    make token

--- src/main/java/com/example/demo/domain/member/Service/MemberService.java
+++ src/main/java/com/example/demo/domain/member/Service/MemberService.java
@@ -1,11 +1,20 @@
 package com.example.demo.domain.member.Service;
 
-import com.example.demo.domain.member.repository.MemberRepository;
+import com.example.demo.domain.member.controller.GlobalException;
 import com.example.demo.domain.member.entity.Member;
+import com.example.demo.domain.member.repository.MemberRepository;
+import com.example.demo.global.rsdata.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
 
@@ -14,11 +23,16 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final MemberService memberService;
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
     public Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
+    }
+
+    public boolean passwordMatches(Member member, String password) {
+        return passwordEncoder.matches(member.getPassword(), password);
     }
 
     @Transactional
@@ -28,6 +42,68 @@ public class MemberService {
                 .password(passwordEncoder.encode(password))
                 .build();
 
-                memberRepository.save(member);
+        memberRepository.save(member);
+    }
+    @AllArgsConstructor
+    @Getter
+    public static class LoginResponseBody {
+        @NotBlank
+        private String refreshToken;
+        @NotBlank
+        private String accessToken;
+    }
+
+    @Getter
+    @Setter
+    public static class LoginRequestBody {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @PostMapping("/login")
+    public RsData<LoginResponseBody> login(
+            @Valid @RequestBody LoginRequestBody body
+    ) {
+        RsData<MemberService.AuthAndMakeTokensResponseBody> authAndMakeTokensRs = memberService.authAndMakeTokens(body.getUsername(), body.getPassword());
+
+        return RsData.of(
+                authAndMakeTokensRs.getResultCode(),
+                authAndMakeTokensRs.getMsg(),
+                new LoginResponseBody(
+                        authAndMakeTokensRs.getData().getRefreshToken(),
+                        authAndMakeTokensRs.getData().getAccessToken()
+                )
+        );
+    }
+
+    @Getter
+    public static class AuthAndMakeTokensResponseBody {
+        private String accessToken;
+        private String refreshToken;
+
+        public AuthAndMakeTokensResponseBody(String accessToken, String refreshToken) {
+            this.accessToken = accessToken;
+            this.refreshToken = refreshToken;
+        }
+    }
+
+    @Transactional
+    public RsData<AuthAndMakeTokensResponseBody> authAndMakeTokens(String username, String password) {
+        Member member = memberService.findByUsername(username)
+                .orElseThrow(() -> new GlobalException("400-1", "해당 유저가 존재하지 않습니다."));
+
+        if (!passwordMatches(member, password))
+            throw new GlobalException("400-2", "비밀번호가 일치하지 않습니다.");
+
+        String refreshToken = "refreshToken";
+        String accessToken = "accessToken";
+
+        return RsData.of(
+                "200-1",
+                "로그인 성공",
+                new AuthAndMakeTokensResponseBody(accessToken, refreshToken)
+        );
     }
 }
